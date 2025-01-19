18cfd8abbe7fc96a1558b67cf763dd3017661100
Wesley Knight <31393997+clemsonwesley@users.noreply.github.com>
2025-01-18 21:59:02 -0500

    init
    initial commit

---
+++ Source/BowlMVP1/BowlMVP1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BowlMVP1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BowlMVP1, "BowlMVP1" );
